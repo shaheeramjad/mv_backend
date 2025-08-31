44cd9566766b4726d02baec412a389d681b5f195
Shaheer Amjad <shaheeransari450@gmail.com>
2025-08-31 16:08:39 +0500

    Part 1 Completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(
   cors({
     origin: ["http://localhost:5173"],
